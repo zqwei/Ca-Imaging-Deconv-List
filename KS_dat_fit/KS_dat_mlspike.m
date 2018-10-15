f8b0a8c3dbfd12ce1538359046687d180052300d
Ziqiang Wei <weiz@janelia.hhmi.org>
2018-10-15 13:34:48 -0400

    add code for testing and fitting mlspike in c2s models

---
+++ KS_dat_fit/KS_dat_mlspike.m
@@ -0,0 +1,16 @@
+function KS_dat_mlspike(nCell)
+addpath('../fit_code/')
+load('DataListCells.mat')
+warning('off', 'all');
+
+dat     = totCell(nCell);
+dff     = double(dat.dff);
+caTime  = double(dat.CaTime);
+dt      = caTime(2) - caTime(1);
+fr      = 1/dt;
+
+est     = mlspike(dff, fr);
+
+save(['MLSpike_fit_Cell_' num2str(nCell)], 'est')
+
+end
